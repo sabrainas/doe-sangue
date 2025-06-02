e60fa7478d7fd4198c43ab75f2884bebb68134eb
sabrainas <sazonass.m@gmail.com>
2025-06-01 21:34:55 -0300

    pqp agoria vai

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "NEXT_PUBLIC_API_URL=https://hemolink-9fau.onrender.com/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://hemolink-9fau.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
